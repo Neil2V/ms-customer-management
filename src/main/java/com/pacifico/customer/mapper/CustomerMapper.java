8908a8f073475893e0686d83a11325a4ec3fcb7a
Neil2V <49854178+Neil2V@users.noreply.github.com>
2026-01-14 07:44:00 -0500

    add: agrego ajuste sonar

--- src/main/java/com/pacifico/customer/mapper/CustomerMapper.java
+++ src/main/java/com/pacifico/customer/mapper/CustomerMapper.java
@@ -4,10 +4,15 @@ import com.pacifico.customer.model.dto.CustomerRequest;
 import com.pacifico.customer.model.dto.CustomerResponse;
 import com.pacifico.customer.model.entity.CustomerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "columnaCreada", ignore = true)
     CustomerEntity toEntity(CustomerRequest request);
     CustomerResponse toResponse(CustomerEntity entity);
 }
